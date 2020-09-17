dae4221a1be925f06d03676ce830b857ed871e55
Denis Gorshkov <satrasmatrassatras@gmail.com>
2020-09-18 01:50:26 +0300

    Дорабатывает логику и стилизацию

--- src/App.js
+++ src/App.js
@@ -4,78 +4,81 @@ import './App.css';
 import NumberWidget from './NumberWidget'
 
 export class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          currentValue: 10,
-          minValue: 0,
-          maxValue: 20
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleMinValChange = this.handleMinValChange.bind(this);
-        this.handleMaxValChange = this.handleMaxValChange.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentValue: 10,
+      minValue: 0,
+      maxValue: 20
+    };
+  }
 
-    handleChange(newVal) {
-      this.setState({currentValue: newVal});
-    }
-    handleMinValChange(newVal) {
+  handleChange = (newVal) => {
+    this.setState({currentValue: newVal});
+  };
+
+  handleMinValChange = (newVal) => {
+    if (newVal <= this.state.currentValue) {
       this.setState({minValue: newVal});
     }
-    handleMaxValChange(newVal) {
+  };
+
+  handleMaxValChange = (newVal) => {
+    if (newVal >= this.state.currentValue) {
       this.setState({maxValue: newVal});
     }
+  };
 
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                </header>
-                <main className="App-content">
-                    <div className="App-config">
-                      <h2>Widget Config</h2>
-                      <div>
-                        <h3>Current Value</h3>
-                        <NumberWidget
-                          currentValue={this.state.currentValue}
-                          minValue={this.state.minValue}
-                          maxValue={this.state.maxValue}
-                          handleChange={this.handleChange}
-                        />
-                      </div>
-                      <div>
-                        <h3>Min Value</h3>
-                        <NumberWidget
-                          currentValue={this.state.minValue}
-                          minValue={-5}
-                          maxValue={20}
-                          handleChange={this.handleMinValChange}
-                        />
-                      </div>
-                      <div>
-                        <h3>Max Value</h3>
-                        <NumberWidget
-                          currentValue={this.state.maxValue}
-                          minValue={20}
-                          maxValue={50}
-                          handleChange={this.handleMaxValChange}
-                        />
-                      </div>
-                    </div>
-                    <div className="App-demonstration">
-                        <h2>Widget</h2>
-                        <NumberWidget
-                            currentValue={this.state.currentValue}
-                            minValue={this.state.minValue}
-                            maxValue={this.state.maxValue}
-                            handleChange={this.handleChange}
-                        />
-                    </div>
-                </main>
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+        </header>
+        <main className="App-content">
+          <div className="App-config">
+            <h2>Widget Config</h2>
+            <div>
+              <h3>Current Value</h3>
+              <NumberWidget
+                currentValue={this.state.currentValue}
+                minValue={this.state.minValue}
+                maxValue={this.state.maxValue}
+                handleChange={this.handleChange}
+              />
             </div>
-        )
-    }
+            <div>
+              <h3>Min Value</h3>
+              <NumberWidget
+                currentValue={this.state.minValue}
+                minValue={-5}
+                maxValue={20}
+                handleChange={this.handleMinValChange}
+              />
+            </div>
+            <div>
+              <h3>Max Value</h3>
+              <NumberWidget
+                currentValue={this.state.maxValue}
+                minValue={20}
+                maxValue={50}
+                handleChange={this.handleMaxValChange}
+              />
+            </div>
+          </div>
+          <div className="App-demonstration">
+            <h2>Widget</h2>
+            <NumberWidget
+              currentValue={this.state.currentValue}
+              minValue={this.state.minValue}
+              maxValue={this.state.maxValue}
+              handleChange={this.handleChange}
+            />
+          </div>
+        </main>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/NumberWidget.js
+++ src/NumberWidget.js
@@ -4,65 +4,57 @@ import MinusIcon from './img/qty-minus-icon.svg'
 import './NumberWidget.css';
 
 export default class NumberWidget extends React.Component {
-    constructor(props) {
-        super(props);
-        this.onValueChange = this.onValueChange.bind(this);
-        this.onButtonClick = this.onButtonClick.bind(this);
-        this.onWheel = this.onWheel.bind(this);
-        this.isValid = this.isValid.bind(this);
+  isValid = (val) => {
+    const {minValue, maxValue} = this.props;
+    return minValue <= val && maxValue >= val;
+  };
+
+  onValueChange = (e) => {
+    const newVal = parseInt(e.target.value);
+    if (this.isValid(newVal)) {
+      this.props.handleChange(newVal);
     }
+  };
 
-    isValid(val) {
-        const {minValue, maxValue} = this.props;
-        return minValue <= val && maxValue >= val;
+  onButtonClick = (newVal) => {
+    if (this.isValid(newVal)) {
+      this.props.handleChange(newVal);
     }
+  };
 
-    onValueChange(e) {
-        const newVal = parseInt(e.target.value);
-        if (this.isValid(newVal)) {
-            this.props.handleChange(newVal);
-        }
-    }
+  onWheel = () => {};
 
-    onButtonClick(newVal) {
-        if (this.isValid(newVal)) {
-            this.props.handleChange(newVal);
-        }
-    }
+  render() {
+    const {currentValue} = this.props;
 
-    onWheel(e) {}
-
-    render() {
-        const {currentValue} = this.props;
-
-        return (
-            <div className="number">
-                <button
-                    className="number__button number__button--minus button"
-                    type="button"
-                    aria-label="Понизить на 1"
-                    onClick={() => this.onButtonClick(currentValue - 1)}
-                >
-                    <img className="number__icon" src={MinusIcon} alt="plus-icon" width="20" height="10"/>
-                </button>
-                <input
-                    className="number__input"
-                    type="number"
-                    name="companions-count"
-                    id="companions-count-field"
-                    value={this.props.currentValue}
-                    onChange={this.onValueChange}
-                    onWheel={this.onWheel}
-                />
-                <button
-                    className="number__button number__button--plus button"
-                    type="button"
-                    aria-label="Увеличить на 1"
-                    onClick={() => this.onButtonClick(currentValue + 1)}
-                >
-                    <img className="number__icon" src={PlusIcon} alt="plus-icon" width="20" height="20"/>
-                </button>
-            </div>
-        );
-    }
+    return (
+      <div className="number">
+        <button
+          className="number__button number__button--minus button"
+          type="button"
+          aria-label="Понизить на 1"
+          onClick={() => this.onButtonClick(currentValue - 1)}
+        >
+          <img className="number__icon" src={MinusIcon} alt="plus-icon" width="20" height="10"/>
+        </button>
+        <input
+          className="number__input"
+          type="number"
+          name="companions-count"
+          id="companions-count-field"
+          value={currentValue}
+          onChange={this.onValueChange}
+          onWheel={this.onWheel}
+        />
+        <button
+          className="number__button number__button--plus button"
+          type="button"
+          aria-label="Увеличить на 1"
+          onClick={() => this.onButtonClick(currentValue + 1)}
+        >
+          <img className="number__icon" src={PlusIcon} alt="plus-icon" width="20" height="20"/>
+        </button>
+      </div>
+    );
+  }
 }
